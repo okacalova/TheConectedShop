dea2b181f50b11e85cb8cae89798b9860d731119
Олеся Качалова <olesakacalova@MacBook-Pro-Olesa.local>
2024-08-18 20:07:06 +0300

    homepage v1

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -14,7 +14,10 @@ test('Open Home Page and check title',async ({ page }) => {
 test('Logo is displayed',async({page}) =>{
 const Logo = await page.$('.Header__LogoImage--primary');
 expect(Logo).toBeTruthy();
-})
+const LogoFront = await page.$('.Header__LogoImage Header--transparent');
+expect(LogoFront).toBeTruthy();
+expect (await LogoFront.isVisible()).toBeTruthy();
+});
 
 test('Navigation main are displayed', async({page}) => {
     const navLinks=[
@@ -25,7 +28,19 @@ test('Navigation main are displayed', async({page}) => {
     for (const link of navLinks) {
         const linkElement = await page.$(`a.${link.className}:has-text("${link.text}")`);
         expect(linkElement).toBeTruthy();
-      }
-    });
-    
+      
+ }
+});
+   test('searchBar',async({page}) => {
+    const search = await page.$('a[data-action="toggle-search"]');
+    expect (search).toBeTruthy();
+    await search.click();
+ });
+
+   test('searchInput2',async({page}) => {
+   const searchInput = await page.$('input[type="search"]');
+   expect (searchInput).toBeTruthy();
+        await searchInput.click(); 
+   
+ } );
 });
